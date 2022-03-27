bc52f07151a1ae723feec3295c3c2f005956d4fd
zen <zen@zen>
2022-03-27 09:54:15 -0700

    new docker files

--- go_echo_server/main.go
+++ go_echo_server/main.go
@@ -19,9 +19,13 @@ func main() {
 		return c.HTML(http.StatusOK, "Hello World")
 	})
 
-	//	e.GET("/ping", func(c echo.Context) error {
-	//		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	//	})
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Hello, Docker! <3")
+	})
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
 
 	httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
